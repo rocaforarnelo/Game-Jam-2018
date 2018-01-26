86a1284184a3a5eee0956c1054159630a3aaf650
Kristoffer Lirazan <lirazan.kristoffer@gmail.com>
2018-01-27 00:55:08 +0800

    trailUPdate

---
+++ Game Jam 2018/Assets/__KRIS/Scripts/GestureMouse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestureMouse : MonoBehaviour {
+
+    public GameObject MouseRenderer;
+
+    private void Update()
+    {
+        MouseTrail();
+    }
+
+    void MouseTrail()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        MouseRenderer.transform.position = mousePos;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            MouseRenderer.SetActive(true);
+            MouseRenderer.GetComponent<TrailRenderer>().time = 5;
+            MouseRenderer.GetComponent<TrailRenderer>().widthMultiplier = 5f;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            MouseRenderer.GetComponent<TrailRenderer>().time = 0f;
+            MouseRenderer.GetComponent<TrailRenderer>().widthMultiplier = 0f;
+        }
+    }
+
+}
